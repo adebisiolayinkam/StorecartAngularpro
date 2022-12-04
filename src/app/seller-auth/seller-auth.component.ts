47e295efaedef1bae2fe6291462e979a6235372c
adebisiolayinkam <39669565+adebisiolayinkam@users.noreply.github.com>
2022-12-04 13:04:56 +0100

    step 1-9

---
+++ src/app/seller-auth/seller-auth.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { SellerService } from '../services/seller.service';
+import {Router} from '@angular/router';
+import { SignUp } from '../data-type';
+
+@Component({
+  selector: 'app-seller-auth',
+  templateUrl: './seller-auth.component.html',
+  styleUrls: ['./seller-auth.component.css']
+})
+export class SellerAuthComponent implements OnInit {
+
+ //constructor(private seller: SellerService, private router: Router) {}
+  constructor(private seller: SellerService) {}
+
+   showLogin=false
+  ngOnInit():void{
+    this.seller.reloadSeller()
+  }
+
+ signUp(data:SignUp):void{
+    console.warn(data);
+    this.seller.userSignUp(data);
+ }
+
+ login(data:SignUp):void{
+  console.warn(data);
+ 
+}
+
+ openLogin(){
+    this.showLogin=true
+ }
+
+ openSignUp(){
+  this.showLogin= false
+ }
+
+}
