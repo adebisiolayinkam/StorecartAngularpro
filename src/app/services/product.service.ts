ec260b29a037e2444127b80ebbf122b19f535004
adebisiolayinkam <39669565+adebisiolayinkam@users.noreply.github.com>
2022-12-06 00:13:04 +0100

    step 16

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -12,7 +12,11 @@ export class ProductService {
     return this.http.post('http://localhost:3000/products', data);
   }
 
-  productList(){
+  productList() {
     return this.http.get<product[]>('http://localhost:3000/products');
+  }
+
+  deleteProduct(id: number) {
+   return this.http.delete(`http://localhost:3000/products/${id}`);
   }
 }
