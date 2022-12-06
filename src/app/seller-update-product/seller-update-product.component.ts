d07d2564a4d7d27f10f4818c7cff79d05d8b3d02
adebisiolayinkam <39669565+adebisiolayinkam@users.noreply.github.com>
2022-12-06 02:17:06 +0100

    step 19

--- src/app/seller-update-product/seller-update-product.component.ts
+++ src/app/seller-update-product/seller-update-product.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { product } from '../data-type';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-seller-update-product',
   templateUrl: './seller-update-product.component.html',
-  styleUrls: ['./seller-update-product.component.css']
+  styleUrls: ['./seller-update-product.component.css'],
 })
-export class SellerUpdateProductComponent implements OnInit{
-
-  constructor() {}
+export class SellerUpdateProductComponent implements OnInit {
+  productData: undefined | product;
+  constructor(private route: ActivatedRoute, private product: ProductService) {}
 
   ngOnInit(): void {
-    
+    let productId = this.route.snapshot.paramMap.get('id');
+    console.warn(productId);
+    productId &&
+      this.product.getProduct(productId).subscribe((data) => {
+        console.warn(data);
+        this.productData = data;
+      });
   }
 
-  submit(data:any){
-
-  }
+  submit(data: any) {}
 }
